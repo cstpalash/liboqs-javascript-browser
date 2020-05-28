bee0b8959fb3f739332b591d8be8e4ed262f65a1
Palash Roy <cstpalash@gmail.com>
2020-05-28 13:14:50 +0530

    demo

--- demo/config.js
+++ demo/config.js
@@ -1,10 +1,4 @@
 module.exports = {
 	server: 'localhost',
-	port : 3000,
-	interval : 5000,
-
-	api : {
-		live : 'api/live',
-		publicKey : 'api/publickey'
-	}
+	port : 3000
 }
--- demo/server.js
+++ demo/server.js
@@ -1,23 +1,54 @@
+const opn = require('opn');
 const express = require('express');
 const app = express();
 var colors = require('colors');
 var config = require('./config.js');
 var path = require('path');
+colors.setTheme({ info:'brightMagenta', highlight:'brightBlue', error:'brightRed', success:'green'});
 
-colors.setTheme({ info:'brightMagenta', warning:'brightYellow', error:'brightRed', success:'brightGreen'});
+app.use(express.static('demo/public'));
+app.get("/api/qs/crypto", getCrypto);
 
+const algo = "FrodoKEM-640-AES";
 
-app.get(`/${config.api.live}`, live); //live api
+var qsServer = require('../javascript/liboqs-server.js');
+var publicKey = null;
 
-app.get(`/${config.api.publicKey}`, getPublicKey); //live api
+qsServer["onRuntimeInitialized"] = function(){
 
-app.listen(config.port, () => console.log(`Demo server is listening at http://${config.server}:${config.port}`.info));
+	var keyGenerationStatus = qsServer.ccall('GenerateKeyPair', 'number', ['string'], [ algo ]);
+	
+	if(keyGenerationStatus == 0){
+		console.log(`Algo : ${algo} => public/secret key pair generation successful`.success);
+		publicKey = qsServer.ccall('GetPublicKey', 'string');
 
+		console.log(`Public Key (size:${Buffer.from(publicKey, 'base64').length}) : ${wrap(publicKey, 50)}`.highlight);
 
-function live(req, res){
-	return res.json({ live : true });
+		var url = `http://${config.server}:${config.port}`;
+		app.listen(config.port, () => console.log(`Demo server is listening at ${url}`.info));
+
+		opn(url);
+	}
+	else{
+
+		console.log(`Algo : ${algo} => public/secret key pair generation failed`.error);
+
+	}
 }
 
-function getPublicKey(req, res){
-	return res.sendFile(path.join(__dirname, '../public-key'));
+function wrap(input, limit){
+	if(input.length <= limit) return input;
+
+	return input.substring(0, limit) + '...';
+}
+
+
+function getCrypto(req, res){
+	return res.json({
+		err: null,
+		data: {
+			algo : algo,
+			publicKey : publicKey
+		}
+	});
 }
